4307ca5def43bce35d7a22e848de3f4ab471dc6e
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-03-25 17:08:25 -0500

    Changed icons and added to manifest for the splash screen

--- app/src/main/java/com/globant/superappexample/MainActivity.kt
+++ app/src/main/java/com/globant/superappexample/MainActivity.kt
@@ -11,12 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.globant.superappexample.ui.theme.SuperAppExampleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             SuperAppExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
