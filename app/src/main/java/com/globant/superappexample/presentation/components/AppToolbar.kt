9b5eaf7f83eef0beed707e8ed5790733aeb16a3d
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-03-31 19:14:24 -0500

    Added Icon

--- app/src/main/java/com/globant/superappexample/presentation/components/AppToolbar.kt
+++ app/src/main/java/com/globant/superappexample/presentation/components/AppToolbar.kt
@@ -1,4 +1,30 @@
 package com.globant.superappexample.presentation.components
 
-class AppToolbar {
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.globant.superappexample.R
+
+@Composable
+fun AppToolbar(innerPadding: PaddingValues) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(innerPadding)
+    ) {
+        Icon(
+            modifier = Modifier.size(40.dp),
+            painter = painterResource(id = R.drawable.ic_inventory_app),
+            contentDescription = "Icon image"
+        )
+    }
 }
