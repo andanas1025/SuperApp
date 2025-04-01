24d2a7ec5f270514c58d7ba8aa49d0ca97593710
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-03-31 20:04:50 -0500

    Changed the Init Composable function in MainActivity

--- app/src/main/java/com/globant/superappexample/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/globant/superappexample/presentation/activities/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.globant.superappexample.presentation.components.AppToolbar
 import com.globant.superappexample.presentation.ui.theme.SuperAppExampleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperAppExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppToolbar(innerPadding)
                 }
             }
         }
