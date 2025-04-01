7b50fd4154f9a11da036095739e804ece907357c
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-01 11:50:19 -0500

    Change app toolbar style and colors

--- app/src/main/java/com/globant/superappexample/presentation/components/AppToolbar.kt
+++ app/src/main/java/com/globant/superappexample/presentation/components/AppToolbar.kt
@@ -1,5 +1,6 @@
 package com.globant.superappexample.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -21,20 +23,25 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.globant.superappexample.R
 import com.globant.superappexample.presentation.ui.theme.SuperAppExampleTheme
+import com.globant.superappexample.presentation.ui.theme.primaryColor
+import com.globant.superappexample.presentation.ui.theme.whiteColor
 
 @Composable
 fun AppToolbar() {
     Row(
         modifier = Modifier
+            .background(primaryColor)
+            .systemBarsPadding()
             .fillMaxWidth()
             .height(70.dp)
             .padding(18.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier.size(20.dp),
             painter = painterResource(id = R.drawable.ic_inventory_app),
-            contentDescription = "Icon image"
+            contentDescription = "Icon image",
+            tint = whiteColor
         )
 
         SpaceWidth()
@@ -42,15 +49,17 @@ fun AppToolbar() {
         TextComponent(
             textValue = stringResource(R.string.add_address),
             modifier = Modifier.wrapContentSize(),
-            fontSizeValue = 20.sp
+            fontSizeValue = 20.sp,
+            textColorValue = whiteColor
         )
 
         Spacer(modifier = Modifier.weight(1f))
 
         Icon(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier.size(20.dp),
             painter = painterResource(id = R.drawable.ic_inventory_app),
-            contentDescription = "Icon image"
+            contentDescription = "Icon image",
+            tint = whiteColor
         )
     }
 }
@@ -58,9 +67,5 @@ fun AppToolbar() {
 @Preview(showBackground = false)
 @Composable
 fun AppToolbarPreview() {
-    SuperAppExampleTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            AppToolbar()
-        }
-    }
+    AppToolbar()
 }
