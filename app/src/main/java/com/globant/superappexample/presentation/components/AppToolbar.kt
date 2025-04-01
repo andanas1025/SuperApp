7edc13084d9d4c33e646c6e6cea4dc064d77640b
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-03-31 23:22:58 -0500

    Added new components inside the toolbar from the design system

--- app/src/main/java/com/globant/superappexample/presentation/components/AppToolbar.kt
+++ app/src/main/java/com/globant/superappexample/presentation/components/AppToolbar.kt
@@ -7,14 +7,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.globant.superappexample.R
 import com.globant.superappexample.presentation.ui.theme.SuperAppExampleTheme
 
@@ -32,10 +35,18 @@ fun AppToolbar(innerPadding: PaddingValues) {
             painter = painterResource(id = R.drawable.ic_inventory_app),
             contentDescription = "Icon image"
         )
+
+        SpaceWidth()
+
+        TextComponent(
+            textValue = stringResource(R.string.add_address),
+            modifier = Modifier.wrapContentSize(),
+            fontSizeValue = 20.sp
+        )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun AppToolbarPreview() {
     SuperAppExampleTheme {
