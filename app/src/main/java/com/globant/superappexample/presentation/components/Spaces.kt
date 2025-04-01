6ca7d88b39d1056531c4240cabb0a8ed0ab25b9f
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-03-31 23:22:34 -0500

    Added Spaces component

--- app/src/main/java/com/globant/superappexample/presentation/components/Spaces.kt
+++ app/src/main/java/com/globant/superappexample/presentation/components/Spaces.kt
@@ -1,2 +1,19 @@
 package com.globant.superappexample.presentation.components
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun MainSpace(size: Dp) {
+    Spacer(modifier = Modifier.width(size))
+}
+
+@Composable
+fun SpaceWidth() {
+    MainSpace(18.dp)
+}
