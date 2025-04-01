2aa19f55f73b8544b40042377c5be5079484cf85
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-01 12:28:32 -0500

    Added box and some label in banner component

---
+++ app/src/main/java/com/globant/superappexample/presentation/components/BannerComponent.kt
@@ -0,0 +1,68 @@
+package com.globant.superappexample.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun BannerComponent(
+    title: String? = null,
+    description: String? = null,
+    image: String? = null
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(240.dp)
+            .padding(12.dp)
+            .background(Color.Green),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            title?.let {
+                TextComponent(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(8.dp),
+                    textValue = title,
+                    fontSizeValue = 24.sp
+                )
+            }
+
+            description?.let {
+                TextComponent(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(8.dp),
+                    textValue = it
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BannerComponentPreview() {
+    BannerComponent(
+        title = "Hello World",
+        description = "description"
+    )
+}
