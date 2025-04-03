4061f4729f73655b68d0eac36a7ad595d20f223a
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-03 13:31:13 -0500

    Added drawable at the center end of the banner component

--- app/src/main/java/com/globant/superappexample/presentation/components/BannerComponent.kt
+++ app/src/main/java/com/globant/superappexample/presentation/components/BannerComponent.kt
@@ -1,6 +1,5 @@
 package com.globant.superappexample.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,13 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +42,15 @@ fun BannerComponent(
                 model = it,
                 contentDescription = "Banner image",
                 contentScale = ContentScale.Crop
+            )
+        }
+        resourceValue?.let {
+            ImageComponent(
+                modifier = Modifier
+                    .size(120.dp)
+                    .padding(18.dp)
+                    .align(Alignment.CenterEnd),
+                resourceValue = it
             )
         }
         Column(
--- app/src/main/java/com/globant/superappexample/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/globant/superappexample/presentation/screens/home/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.globant.superappexample.R
 import com.globant.superappexample.presentation.components.AppToolbar
 import com.globant.superappexample.presentation.components.BannerComponent
 import com.globant.superappexample.presentation.ui.theme.whiteColor
@@ -31,7 +32,8 @@ fun HomeScreen() {
             BannerComponent(
                 title = "Hello World",
                 description = "description",
-                image = "https://images.unsplash.com/photo-1742403949587-42a767b9ea5b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+                image = "https://images.unsplash.com/photo-1742403949587-42a767b9ea5b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D",
+                resourceValue = R.drawable.ic_inventory_app
             )
         }
     }
