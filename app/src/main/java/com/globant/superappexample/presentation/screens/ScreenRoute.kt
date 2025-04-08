3e23412c303f11782801cdb174d044cf92b7e715
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-07 22:20:21 -0500

    Created new Screen Route to be used on App navGraph

--- app/src/main/java/com/globant/superappexample/presentation/screens/ScreenRoute.kt
+++ app/src/main/java/com/globant/superappexample/presentation/screens/ScreenRoute.kt
@@ -2,4 +2,5 @@ package com.globant.superappexample.presentation.screens
 
 sealed class ScreenRoute(val route: String) {
     data object HomeScreen : ScreenRoute(route = "home_screen")
+    data object WealthyScreen : ScreenRoute(route = "Wealthy_screen")
 }
