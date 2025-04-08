07b1e064588e65bb4a756337f5666dcb0474b209
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-08 18:01:00 -0500

    hilt provide the retrofit instance

--- wealthy/src/main/java/com/andanas/wealthy/data/remote/repository/iCoinRepository.kt
+++ wealthy/src/main/java/com/andanas/wealthy/data/remote/repository/iCoinRepository.kt
@@ -1,6 +1,5 @@
 package com.andanas.wealthy.data.remote.repository
 
-import com.andanas.wealthy.data.remote.dto.CoinDto
 import com.andanas.wealthy.domain.model.Coin
 import com.andanas.wealthy.domain.model.CoinDetail
 import com.globant.utilities.Resource
--- wealthy/src/main/java/com/andanas/wealthy/di/WealthModule.kt
+++ wealthy/src/main/java/com/andanas/wealthy/di/WealthModule.kt
@@ -1,9 +1,41 @@
 package com.andanas.wealthy.di
 
+import com.andanas.wealthy.data.remote.CoinApi
+import com.globant.utilities.constants.AppConstants
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 class WealthModule {
 
+    @Provides
+    @Singleton
+    fun provideCoinPaprikaApi(): CoinApi {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient().newBuilder()
+        httpClient.addInterceptor(httpLoggingInterceptor)
+
+        val interceptor = Interceptor { chain ->
+            val request = chain.request().newBuilder().build()
+            chain.proceed(request)
+        }
+
+        httpClient.addInterceptor(interceptor)
+
+        return Retrofit.Builder()
+            .baseUrl(AppConstants.BASE_COIN_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build()
+            .create(CoinApi::class.java)
+    }
 }
