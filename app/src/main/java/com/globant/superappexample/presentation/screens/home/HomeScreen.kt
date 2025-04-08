49db372faee11b590aa764578bf80a54a2a8a6cc
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-07 22:23:30 -0500

    Added anonym function to handle the clicks

--- app/src/main/java/com/globant/superappexample/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/globant/superappexample/presentation/screens/home/HomeScreen.kt
@@ -9,17 +9,27 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.globant.superappexample.R
+import com.andanas.design.R.drawable.ic_inventory_app
 import com.andanas.design.components.AppToolbar
 import com.andanas.design.components.BannerComponent
 import com.andanas.design.ui.theme.whiteColor
+import com.globant.superappexample.R
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    primaryButtonClicked: () -> Unit = {},
+    wealthBannerClicked: () -> Unit = {}
+) {
     Scaffold(
         topBar = {
-            AppToolbar()
+            AppToolbar(
+                title = stringResource(R.string.home_screen_title),
+                isBackButtonVisible = false,
+                isNotificationVisible = true,
+                primaryButtonClicked = primaryButtonClicked
+            )
         }
     ) { innerPadding ->
         Column(
@@ -30,10 +40,13 @@ fun HomeScreen() {
                 .padding(innerPadding)
         ) {
             BannerComponent(
-                title = "Hello World",
-                description = "description",
+                title = "Wealthy Section",
+                description = "Goes to Wealthy section to see more information",
                 image = "https://images.unsplash.com/photo-1742403949587-42a767b9ea5b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D",
-                resourceValue = R.drawable.ic_inventory_app
+                resourceValue = R.drawable.ic_launcher_foreground,
+                bannerClicked = {
+                    wealthBannerClicked()
+                }
             )
         }
     }
