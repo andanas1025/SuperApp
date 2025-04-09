46d1e4a31f3a148cb3ed391e9c34023d29edd2aa
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-09 16:58:08 -0500

    Use Case coin added

--- wealthy/src/main/java/com/andanas/wealthy/data/repository/CoinRepositoryImpl.kt
+++ wealthy/src/main/java/com/andanas/wealthy/data/repository/CoinRepositoryImpl.kt
@@ -24,7 +24,7 @@ class CoinRepositoryImpl @Inject constructor(
         } catch (e: HttpException) {
             emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
         } catch (e: IOException) {
-            emit(Resource.Error("Couldn't reach server. Check your internet connection.")
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
         }
     }
 
---
+++ wealthy/src/main/java/com/andanas/wealthy/domain/useCase/GetCoinsUseCase.kt
@@ -0,0 +1,16 @@
+package com.andanas.wealthy.domain.useCase
+
+import com.andanas.wealthy.domain.model.Coin
+import com.andanas.wealthy.domain.repository.ICoinRepository
+import com.globant.utilities.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val coinRepository: ICoinRepository
+) {
+    suspend operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        coinRepository.getCoins()
+    }
+}
