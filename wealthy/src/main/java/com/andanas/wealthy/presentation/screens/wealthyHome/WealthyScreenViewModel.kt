5fe382824fcf41fb1cdae99e5e9495de0a806261
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-04-09 17:26:02 -0500

    Wealthy View Model and state flow added

---
+++ wealthy/src/main/java/com/andanas/wealthy/presentation/screens/wealthyHome/WealthyScreenViewModel.kt
@@ -0,0 +1,44 @@
+package com.andanas.wealthy.presentation.screens.wealthyHome
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.andanas.wealthy.domain.useCase.GetCoinsUseCase
+import com.globant.utilities.Resource
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class WealthyScreenViewModel @Inject constructor(
+    private val getCoinsUseCase: GetCoinsUseCase
+) : ViewModel() {
+
+    private val _state = mutableStateOf(WealthyScreenState())
+    val state: State<WealthyScreenState> = _state
+
+    init {
+        viewModelScope.launch {
+            getCoins()
+        }
+    }
+
+    private suspend fun getCoins() {
+        getCoinsUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = WealthyScreenState(coins = result.data ?: emptyList())
+                }
+
+                is Resource.Error -> {
+                    _state.value = WealthyScreenState(error = result.message)
+                }
+
+                is Resource.Loading -> {
+                    _state.value = WealthyScreenState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
